49801cc12c0a1c80d4b6d47346da89025722f959
umerenkovmaksim <umerenkovmaksim7@gmail.com>
2022-12-26 22:56:25 +0300

    Create file with functions, create objects 'Player' and 'Camera'

--- game.py
+++ game.py
@@ -1,4 +1,52 @@
-from player.player import Player
-from enemy.enemy import Enemy
-from world.world import World
+import pygame
 
+from functions import *
+from player.player import Player
+from player.camera import Camera
+
+width, height = 1280, 720
+running = True
+fps = 60
+clock = pygame.time.Clock()
+left, right, up, down = False, False, False, False
+player_group = pygame.sprite.Group()
+all_sprites = pygame.sprite.Group()
+
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Перемещение героя. Камера')
+pygame.init()
+
+camera = Camera(width, height)
+player = Player(0, 0, player_group, all_sprites)
+
+
+while running:
+    for event in pygame.event.get():
+        keys = pygame.key.get_pressed()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                left = True
+            if event.key == pygame.K_RIGHT:
+                right = True
+            if event.key == pygame.K_DOWN:
+                down = True
+            if event.key == pygame.K_UP:
+                up = True
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                left = False
+            if event.key == pygame.K_RIGHT:
+                right = False
+            if event.key == pygame.K_DOWN:
+                down = False
+            if event.key == pygame.K_UP:
+                up = False
+    screen.fill('black')
+    camera.update(player)
+    for sprite in all_sprites:
+        camera.apply(sprite)
+    player_group.update(left, right, up, down)
+    player_group.draw(screen)
+    pygame.display.flip()
+    clock.tick(fps)
--- player/player.py
+++ player/player.py
@@ -1,2 +1,21 @@
-class Player:
-    pass
+import pygame
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, x, y, player_group, all_sprites):
+        super().__init__(player_group, all_sprites)
+        self.speedx = 3
+        self.speedy = 3
+        self.image = pygame.Surface((20, 20))
+        self.image.fill('red')
+        self.rect = self.image.get_rect().move(x, y)  # Координаты спавна персонажа
+
+    def update(self, left, right, up, down):
+        if left:
+            self.rect.x += -self.speedx
+        if right:
+            self.rect.x += self.speedx
+        if up:
+            self.rect.y += -self.speedy
+        if down:
+            self.rect.y += self.speedy
